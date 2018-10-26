82cfe25c700a503c846084a8432d14175a398a14
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2018-10-26 06:52:31 +0600

    file needs to be run on blenders in blender through txt editor

--- blender_console_script.py
+++
@@ -1,4 +0,0 @@
-import bpy
-
-script = bpy.data.texts["/home/soumics/Desktop/Automate_Blender/Tests/LodeVideo.py"]
-exec(script.as_string())
---
+++ blender_runScrips.py
@@ -0,0 +1,2 @@
+filename = "/home/soumics/Desktop/Automate_Blender/Tests/LodeVideo.py"
+exec(compile(open(filename).read(),filename, 'exec'))
