f6ce7006b05194228b614231fc999813d8ef2391
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2018-10-28 17:07:31 +0600

    organizing clips to frame zero

--- Load&Cut.py
+++ Load&Cut.py
@@ -6,18 +6,23 @@ from ast import literal_eval as creatTuple
 VidfilePath = "//Nas Daily about the student movement in Bangladesh right Now 😰-xgqaOFRP0Qk.mkv" # -> Enter Video file $PATH Here
 TxtfilePath = "/home/soumics/Desktop/Automate_Blender/BlenderVideoEdit(py)/a.txt" # -> Enter marker file $PATH Here
 
+frame_ms = []
+
 def detect_fps_Format(fps): # -> get fps format from blender
     length = len(str((fps)))
     # print(length) 
     if (length <= 3 ):
         return (fps * 1000)
     return fps
-        
+
+def frame_length(start, end):
+    return (end - start)    
+
 
 def frame(frame): # -> Gets Frame Number
 
-    print(frame)
-    print(bpy.context.scene.render.fps)
+    # print(frame) # -> debug
+    # print(bpy.context.scene.render.fps) # -> debug
     return int (( detect_fps_Format (bpy.context.scene.render.fps) * int(frame)) / 1000 ** 2)
 
 
@@ -34,28 +39,35 @@ def get_frame_markers(file): # -> Reads the marker txt file and creates a tuple 
                 frame_ms.append(creatTuple(l))
 
 
-def cut_vid(): # -> Cuts Video at specific marked points
+def Edit_vid(): # -> Cuts Video at specific marked points
 
     for x in range((len(frame_ms) - 1), -1, -1): # -> Decriments through the Tuple
 
+        # Innificient
         bpy.ops.sequencer.cut(frame=frame(frame_ms[x][1]), type='SOFT', side='RIGHT')
         bpy.ops.sequencer.delete()
         bpy.ops.sequencer.select_active_side(side='LEFT') 
         bpy.ops.sequencer.cut(frame=frame(frame_ms[x][0]), type='SOFT', side='LEFT')
-
-        #print(frame(frame_ms[x][1])) # -> debug
-        #print(frame(frame_ms[x][0])) # -> debug
     
     # Delete the last Snip
     bpy.ops.sequencer.select_active_side(side='LEFT') 
     bpy.ops.sequencer.delete()
     # print (bpy.context.sequences) # -> debug
 
-    bpy.context.sequences[4].select = True # -> select the audio file
+    bpy.context.sequences[-1].select = True # -> select the audio file
     bpy.ops.sequencer.delete()
 
+    push_to_frame_zer0()
 
     
+def push_to_frame_zer0(): # -> Aligns all the video to start from frame 0
+
+    bpy.context.sequences[0].select = 1 # -> selects clip closest to frame 0
+    bpy.ops.sequencer.snap(frame = 0) # -> moves it to frame zero
+    bpy.context.sequences[-1].select = 0 # -> deslects it [NB. clip indexes swaps after very operations]
+
+    bpy.ops.sequencer.gap_remove(all=True) # -> removes all intermediate gaps 
+
 
 if __name__ == '__main__':
 
@@ -65,10 +77,9 @@ if __name__ == '__main__':
     bpy.context.area.type = 'SEQUENCE_EDITOR' # -> Invokes Sequence Editor
     bpy.ops.sequencer.movie_strip_add(filepath=VidfilePath, show_multiview=False, frame_start=0, channel=1) # -> Loads Video file
 
-    frame_ms = []
-    get_frame_markers(TxtfilePath)
+    get_frame_markers(TxtfilePath) # -> loads maker file
     
-    cut_vid()
+    Edit_vid()
 
     print('>>>>>>>>>done<<<<<<<<<')
 
